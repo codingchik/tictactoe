0fefa04580f9c557b5e39062c34a269beb3cc139
codingchik <anabel.cuevas4@gmail.com>
2014-07-22 09:07:39 -0700

    first commit

---
+++ ttt.js
@@ -0,0 +1,22 @@
+var app = angular.module('tictactoeApp',  []);
+
+app.controller('TicTacController', function ($scope) {
+
+	$scope.board = ['', '', '', ['', '', ''] , ['', '', ''] ]
+
+	//$scope.click = counter 
+
+	//	when counter is odd put 'x' in array/board
+	//	when counter is even set to 'o' in array/board
+
+	//	'x' equals player one, change cell to color red
+	//	'y' equals player two, change cell to color blue
+
+	//win logic
+	// while all cells in arrays
+});
+
+
+
+
+
